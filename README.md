bcdef639aad2e24b5e83eb766fed0da50c22da72
Thevaa Chandereng <chandereng@wisc.edu>
2021-12-06 16:29:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# simon2stage
+
+**Authors**: Thevaa Chandereng and Ying Kuen Cheung
+
+Shiny app for sample size calculation for phase II trials using the Simon's two-stage design. 
+
+#### The app is available at 
+
+License
+------------
+simon2stage is available under the open source [MIT license](http://opensource.org/licenses/MIT).
+
+
---
+++ app.R
@@ -0,0 +1,143 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(DT)
+
+
+# Define UI for application that draws a histogram
+ui = shiny::fluidPage(
+    shiny::titlePanel("Simon's two-stage design"),
+    
+    shiny::sidebarLayout(
+        shiny::sidebarPanel(
+            shiny::h4("Design parameters"),
+            
+            shiny::numericInput(
+                "p0",
+                label = withMathJax("Unacepptable response rate, p0 (null hypothesis):"), 
+                value = 0.1,
+                min = 0,
+                max = 1,
+                step = 0.05
+            ),
+            
+            shiny::numericInput(
+                "p1",
+                label = withMathJax("Acceptable response rate, p1 (alternative hypothesis):"), 
+                value = 0.3,
+                min = 0,
+                max = 1,
+                step = 0.05
+            ),
+            
+            shiny::numericInput(
+                "alpha",
+                label = withMathJax("Type I error,  $\\alpha$ (one-sided):"),
+                value = 0.05,
+                min = 0,
+                max = 1,
+                step = 0.05
+            ),
+            
+            shiny::numericInput(
+                "power",
+                label = withMathJax("Power,  1 - $\\beta$:"),
+                value = 0.80,
+                min = 0,
+                max = 1,
+                step = 0.05
+            ),
+            
+            shiny::numericInput(
+                "n",
+                label = "Maximum Sample Size, n:",
+                value = 100,
+                min = 100,
+                max = 500,
+                step = 1
+            )
+        ),
+        shiny::mainPanel(
+            shiny::h5("Sample size calculation for phase II trials using the Simon's two-stage design"),
+            dataTableOutput("tab1"),
+            
+            shiny::h5("For the Simon optimal design:"),
+            shiny::textOutput("text1"),
+            
+            shiny::plotOutput("plot1"),
+            
+            shiny::h5("Reference:"),
+            h6("Simon R (1989). Optimal two-stage designs for phase II clinical trials, Controlled Clinical Trials 10: 1-10.")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server = function(input, output) {
+    
+    output$tab1 <- renderDataTable({
+        fun <- clinfun::ph2simon(input$p0, input$p1, input$alpha, (1 - input$power), input$n)
+        tab <- rbind(fun$out[which.min(fun$out[, 5]), ], fun$out[1, ])
+        tab <- round(tab, 3)
+        tab <- cbind(" " = c("Optimal", "Minimax"), tab)
+        tab 
+    }, extensions = 'Buttons', 
+    options = list(
+        initComplete = JS(
+            "function(settings, json) {",
+            "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+            "}"),
+        dom = 'Bfrtip',
+        buttons = c('copy', 'csv', 'excel', 'pdf', 'print')), rownames= FALSE)
+    
+    
+    output$text1 <- shiny::renderText({
+        fun <- clinfun::ph2simon(
+            input$p0, 
+            input$p1, 
+            input$alpha, 
+            (1 - input$power), 
+            input$n
+            
+        )
+        paste(
+            "If you see more than",
+            fun$out[which.min(fun$out[, 5]), 1],
+            "responses out of the first",
+            fun$out[which.min(fun$out[, 5]), 2],
+            "participants in the first stage, then accrue to a total of",
+            fun$out[which.min(fun$out[, 5]), 4],
+            "participants. Otherwise if you see",
+            fun$out[which.min(fun$out[, 5]), 1],
+            "or fewer responses out of the first",
+            fun$out[which.min(fun$out[, 5]), 2],
+            "participants in the first stage, then stop the trial. After accruing",
+            fun$out[which.min(fun$out[, 5]), 4],
+            "participants, if you see more than",
+            fun$out[which.min(fun$out[, 5]), 3],
+            "responses, then the intervention is considered worthy of further testing."
+        )
+    })
+    
+    output$plot1 <- shiny::renderPlot({
+        fun <- clinfun::ph2simon(
+            input$p0, 
+            input$p1, 
+            input$alpha, 
+            (1 - input$power), 
+            input$n
+        )
+        graphics::plot(fun)
+    })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
